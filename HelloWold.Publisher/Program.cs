2f2f5a9f24ca30de6fceb0d45a3af1cbb067871e
sedat karasungur <limitless_fearless@hotmail.com>
2021-05-06 17:52:35 +0300

    2-Work Queue Bitti. Hello World projesi üstünde çalışıldı dikkat et

--- HelloWold.Publisher/Program.cs
+++ HelloWold.Publisher/Program.cs
@@ -1,5 +1,6 @@
 ﻿using RabbitMQ.Client;
 using System;
+using System.Linq;
 using System.Text;
 
 namespace HelloWold.Publisher//producer diğer adı bu kısımda mesajı yayınlıyoruz
@@ -18,12 +19,18 @@ namespace HelloWold.Publisher//producer diğer adı bu kısımda mesajı yayınl
 
                 channel.QueueDeclare("Yenikuyruk", true, false,false);//kuyrukadı,durable:false ise kuyruk memoryde tutulu true ise fiziksel olarak bir yere kaydolur. Biz true yaptık.,exclusive:true olursa sadece bu kanal üzerinden bu kuyruhğa erişilir. false ise başka kanallardan da erişilebilir ve biz false yaptık. autodelete: true ise eğer kuyruğa bağlı olan subscriber düşerse kuyruk silinsin demek.False yaptık. Kuyruk oluştur.
 
-                string message = "Merhaba";//mesajımız
 
-                var messagebody = Encoding.UTF8.GetBytes(message);//Rabbitmq ya mesajları byte dizisi olarak göndeririz. Bu sayede herşeyi gönderebiliriz. pdf,resim,word herşey
+                Enumerable.Range(1, 50).ToList().ForEach(x=> {//Tek seferde çalıştığında 50 mesaj atıyor. Bu tek seferde iletilecek mesaj sayısını iyi ayarlamak gerekiyor. Eğer mesajın işlenmesi uzun sürüyorsa daha az sayıda tek seferde göndermek daha mantıklı olacaktır.
 
-                channel.BasicPublish(string.Empty, "Yenikuyruk",null,messagebody);//exchange kullanmadığımız için string.empty olarak boş gönderdik eğer boş gönderirsek rouitingKey kuyruk adımız olmalı bizimki YeniKuyruk, IBasicProperties şimdilik null gönderelim. ve mesajımız
-                Console.WriteLine("mesaj gönderildi.");
+                    string message = $"Message {x}";//mesajımız
+
+                    var messagebody = Encoding.UTF8.GetBytes(message);//Rabbitmq ya mesajları byte dizisi olarak göndeririz. Bu sayede herşeyi gönderebiliriz. pdf,resim,word herşey
+
+                    channel.BasicPublish(string.Empty, "Yenikuyruk", null, messagebody);//exchange kullanmadığımız için string.empty olarak boş gönderdik eğer boş gönderirsek rouitingKey kuyruk adımız olmalı bizimki YeniKuyruk, IBasicProperties şimdilik null gönderelim. ve mesajımız
+                    Console.WriteLine("mesaj gönderildi.");
+                });
+
+                
                 Console.ReadLine();//console kapanmasın ki izleyelim
             }
 
--- HelloWold.Subscriber/Program.cs
+++ HelloWold.Subscriber/Program.cs
@@ -17,24 +17,28 @@ namespace HelloWold.Subscriber//consumer diğer adı
             {
                 var channel = connection.CreateModel();
 
-               // channel.QueueDeclare("Yenikuyruk", true, false, false);//publisher tarafında bu kuyruğun oluştuğundan eminsek burada yazmamıza gerek yok ama burada olması var olan kuyruğu etkilemez fakat kuyruk yoksa burada oluşturur. Ama birebir aynı olamlı eğer burada da oluşturacaksak parametreleri. Aşağıda kuyruğu her türlü çağıracağımızdan bir daha oluşturmanın bir zararı yok
+                // channel.QueueDeclare("Yenikuyruk", true, false, false);//publisher tarafında bu kuyruğun oluştuğundan eminsek burada yazmamıza gerek yok ama burada olması var olan kuyruğu etkilemez fakat kuyruk yoksa burada oluşturur. Ama birebir aynı olamlı eğer burada da oluşturacaksak parametreleri. Aşağıda kuyruğu her türlü çağıracağımızdan bir daha oluşturmanın bir zararı yok
 
+                channel.BasicQos(0, 1, false);//prefetchSize:0 yaptık yani consumer herhangi bir boyuttan veri alabilir.,prefetchCount: consumerların kaç tane mesaj alabileceğini belirtiyoruz. Bool Global ise: false ise kaçtane consumer varsa herbirine verdiğimiz  prefetchCount kadar mesaj iletilir. eğer false yaparsak verdiğimiz prefetchCount tüm consumerlara toplamı  prefetchCount kardar olacak kadar mesaj verilir. Örn: prefetchCount: verdik 3 consumer var. 2,2,1 şeklinde consumerlara toplam 5 olacak şekilde iletilir. Yani ne kadar çok subscriber(Consumer) proje ayaktaysa o kadar dinleme hızlı biter
 
                 var consumer = new EventingBasicConsumer(channel);
 
-                channel.BasicConsume("Yenikuyruk", true, consumer);//kuyruk adı,autoAck:true kuyruk mesajı gönderdiği an silsin kuyruktan false ise mesajın iletildiğinden emin olunduğunda silsin. Biz şimdilk true yapıyoruz.
+                channel.BasicConsume("Yenikuyruk", false, consumer);//kuyruk adı,autoAck:false Work Queue konusu için false yaptık ve eventte yeni tanım var
 
-                consumer.Received += Consumer_Received;// bu şekilde eventi oluşturduk += tab tab Consumer_Received oluştu
+
+                consumer.Received += (object sender, BasicDeliverEventArgs e)=>
+                    {
+                    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                        channel.BasicAck(e.DeliveryTag,false);//false yaptığımız autoAck işlemi için eğer mesaj teslim edildiyse kuyruktan sil demek
+
+                    Console.WriteLine("Gelen Mesaj: " + message);
+
+                };// bu şekilde kullanım daha pratik
 
                 Console.ReadLine();
             }
         }
 
-        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)//oluşan method
-        {
-            var message = Encoding.UTF8.GetString(e.Body.ToArray());
 
-            Console.WriteLine("Gelen Mesaj: "+message);
-        }
     }
 }
