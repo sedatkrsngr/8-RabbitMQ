f040665bacd396cad5b1b53922e1ea86a1f22d2a
sedat karasungur <limitless_fearless@hotmail.com>
2021-05-06 16:38:10 +0300

    1-Hello World Console Projeller oluşturuldu

---
+++ HelloWold.Publisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWold.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HelloWold.Subscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWold.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
