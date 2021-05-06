785469584251b7be540d20bf4b3dd579e55c6e14
sedat karasungur <limitless_fearless@hotmail.com>
2021-05-06 17:21:03 +0300

    1- Hello World Bitti

--- HelloWold.Publisher/Program.cs
+++ HelloWold.Publisher/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
-namespace HelloWold.Publisher
+namespace HelloWold.Publisher//producer diğer adı bu kısımda mesajı yayınlıyoruz
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://orztfhbr:61JVQ4JIieuwkzllmolkOId430Hfzyv4@grouse.rmq.cloudamqp.com/orztfhbr");
+
+            using (var connection = factory.CreateConnection())
+            {
+                var channel = connection.CreateModel();//yeni bir kanal oluşturuyoruz
+
+                channel.QueueDeclare("Yenikuyruk", true, false,false);//kuyrukadı,durable:false ise kuyruk memoryde tutulu true ise fiziksel olarak bir yere kaydolur. Biz true yaptık.,exclusive:true olursa sadece bu kanal üzerinden bu kuyruhğa erişilir. false ise başka kanallardan da erişilebilir ve biz false yaptık. autodelete: true ise eğer kuyruğa bağlı olan subscriber düşerse kuyruk silinsin demek.False yaptık. Kuyruk oluştur.
+
+                string message = "Merhaba";//mesajımız
+
+                var messagebody = Encoding.UTF8.GetBytes(message);//Rabbitmq ya mesajları byte dizisi olarak göndeririz. Bu sayede herşeyi gönderebiliriz. pdf,resim,word herşey
+
+                channel.BasicPublish(string.Empty, "Yenikuyruk",null,messagebody);//exchange kullanmadığımız için string.empty olarak boş gönderdik eğer boş gönderirsek rouitingKey kuyruk adımız olmalı bizimki YeniKuyruk, IBasicProperties şimdilik null gönderelim. ve mesajımız
+                Console.WriteLine("mesaj gönderildi.");
+                Console.ReadLine();//console kapanmasın ki izleyelim
+            }
+
         }
     }
 }
--- HelloWold.Subscriber/Program.cs
+++ HelloWold.Subscriber/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
-namespace HelloWold.Subscriber
+namespace HelloWold.Subscriber//consumer diğer adı
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://orztfhbr:61JVQ4JIieuwkzllmolkOId430Hfzyv4@grouse.rmq.cloudamqp.com/orztfhbr");
+
+            using (var connection = factory.CreateConnection())
+            {
+                var channel = connection.CreateModel();
+
+               // channel.QueueDeclare("Yenikuyruk", true, false, false);//publisher tarafında bu kuyruğun oluştuğundan eminsek burada yazmamıza gerek yok ama burada olması var olan kuyruğu etkilemez fakat kuyruk yoksa burada oluşturur. Ama birebir aynı olamlı eğer burada da oluşturacaksak parametreleri. Aşağıda kuyruğu her türlü çağıracağımızdan bir daha oluşturmanın bir zararı yok
+
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                channel.BasicConsume("Yenikuyruk", true, consumer);//kuyruk adı,autoAck:true kuyruk mesajı gönderdiği an silsin kuyruktan false ise mesajın iletildiğinden emin olunduğunda silsin. Biz şimdilk true yapıyoruz.
+
+                consumer.Received += Consumer_Received;// bu şekilde eventi oluşturduk += tab tab Consumer_Received oluştu
+
+                Console.ReadLine();
+            }
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)//oluşan method
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+            Console.WriteLine("Gelen Mesaj: "+message);
         }
     }
 }
