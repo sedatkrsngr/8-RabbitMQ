5bf7d0dd8cf3cb0991d8bfd09fa3a53063fd9e9b
sedat karasungur <limitless_fearless@hotmail.com>
2021-05-07 16:07:52 +0300

    5- TopicExchange bitti

--- HelloWold.Publisher/Program.cs
+++ HelloWold.Publisher/Program.cs
@@ -6,11 +6,18 @@ using System.Threading;
 
 namespace HelloWold.Publisher//producer diğer adı bu kısımda mesajı yayınlıyoruz
 {
+    public enum lognames
+    {
+        Critical = 1,
+        Error = 2,
+        Warning = 3,
+        Info = 4
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Thread.Sleep(1000);//projeyi aynı anda ayağıkaldırınca fanout mesajları subscriber(consumer) dinlemeden önce paylaşmamak için bu proje 1 sn sonra başlasın dedik.
+            Thread.Sleep(2000);//projeyi aynı anda ayağıkaldırınca fanout mesajları subscriber(consumer) dinlemeden önce paylaşmamak için bu proje 1 sn sonra başlasın dedik.
             var factory = new ConnectionFactory();
 
             factory.Uri = new Uri("amqps://orztfhbr:61JVQ4JIieuwkzllmolkOId430Hfzyv4@grouse.rmq.cloudamqp.com/orztfhbr");
@@ -19,23 +26,33 @@ namespace HelloWold.Publisher//producer diğer adı bu kısımda mesajı yayınl
             {
                 var channel = connection.CreateModel();//yeni bir kanal oluşturuyoruz
 
-                //FanoutExchange de kuyruğu producer(publisher) oluşturmaz. Consumer(subscriber) isterse veri için kuyruk oluşturur. Ve consumer kapandığında consumer ayarına göre kuyruk silinir. Örn: Hava saatlik hava durumu yayınlayan bir projem var exchange ile kaç kişi bu veriye ulaşacak bilmediğim için veriyi isteyen kişiler kuyruk oluşturup veriyi alabilir. Biz sadece yayınlamakla mükellefiz.Bu yüzden kuyruk oluşturma alanı yorum satırı haline getirildi. Fanout veriyi direkt olarak oluşturulmuş her kuyruğa verir. Yani tüm kuyruklar procuder(publisher) dan gelen veriyi aynı dinler. 
+                //Topic Exchange de kuyruğu  producer(publisher) oluşturmaz.  veriyi routekey ile gönderir ve o route key kullanan consumerlar ilgili olan route sahib kuyruklardan veriyi alır. Topicte routekeyler birden fazla olabilir ve aralarında . kullanılır. Bu yüzden birden fazla route key içerisinde belirli değere göre kuyruklar mesajı iletebilir aşağıda örnek mevcut.
+                //örn: Consumer belirli keye ait kuyrukları çağırmak istediğinde aşağıdaki gibi kullanım route keylerde kullanılır.
+                // var routeKey = "*.Error.*";//Ortasında Error olan routekeye sahip kuyrukları dinleyelim
+                //var routeKey = "*.*.Error";//sonunda Error olan routekeye sahip kuyrukları dinleyelim
+               // var routeKey = "Error.#";//Başı Error olan routekeye sahip kuyrukları dinleyelim
 
-                // channel.QueueDeclare("Yenikuyruk", true, false,false);//kuyrukadı,durable:false ise kuyruk memoryde tutulu true ise fiziksel olarak bir yere kaydolur. Biz true yaptık.,exclusive:true olursa sadece bu kanal üzerinden bu kuyruhğa erişilir. false ise başka kanallardan da erişilebilir ve biz false yaptık. autodelete: true ise eğer kuyruğa bağlı olan subscriber düşerse kuyruk silinsin demek.False yaptık. Kuyruk oluştur.
 
-                channel.ExchangeDeclare("logs-fanout",type:ExchangeType.Fanout,durable:true);
 
-                Enumerable.Range(1, 50).ToList().ForEach(x=> {
+                channel.ExchangeDeclare("logs-topic", type: ExchangeType.Topic, durable: true);
 
-                    string message = $"Message {x}";
+                Random random = new Random();
+                Enumerable.Range(1, 50).ToList().ForEach(x =>
+                {
+                    lognames log1 = (lognames)random.Next(1, 5);
+                    lognames log2 = (lognames)random.Next(1, 5);
+                    lognames log3 = (lognames)random.Next(1, 5);
+
+
+                    var routeKey = $"{log1}.{log2}.{log3}";//hangi mesaj hangi kuyruğa gidecek burdan belirliyoruz. Yani key ile veriyi göndereceğimiz kuyruğu biliyoruz. Topicte birden fazla key verebiliyoruz
+                    string message = $"Message-{log1}-{log2}-{log3}";
 
                     var messagebody = Encoding.UTF8.GetBytes(message);
-
-                    channel.BasicPublish("logs-fanout","", null, messagebody);//exchange:logs-fanout , rouitingKey:"", IBasicProperties şimdilik null gönderelim. ve mesajımız
+                    channel.BasicPublish("logs-topic", routeKey, null, messagebody);//exchange:logs-direct , rouitingKey:routeKey, IBasicProperties şimdilik null gönderelim. ve mesajımız // exchange routekeye sahip olan kuyruğa mesajı iletiyor
                     Console.WriteLine("mesaj gönderildi.");
                 });
 
-                
+
                 Console.ReadLine();
             }
 
--- HelloWold.Subscriber/Program.cs
+++ HelloWold.Subscriber/Program.cs
@@ -17,17 +17,17 @@ namespace HelloWold.Subscriber//consumer diğer adı
 
             using (var connection = factory.CreateConnection())
             {
+               // var routeKey = "*.Error.*";//Ortasında Error olan routekeye sahip kuyrukları dinleyelim
+                //var routeKey = "*.*.Error";//sonunda Error olan routekeye sahip kuyrukları dinleyelim
+                var routeKey = "Error.#";//Başı Error olan routekeye sahip kuyrukları dinleyelim
                 var channel = connection.CreateModel();
 
-
-
-                //Eğer burada da exchange oluşturursak hata vermez ama en azından fanout exchange için gereksiz
-
-                var randomQueueName = channel.QueueDeclare().QueueName;//random olarak kuyruk adı oluşturur.
-
-                channel.QueueBind(randomQueueName, "logs-fanout","",null);//Birden fazla  kez projeyi ayağı kaldırınca aynı kuyruk ismi olması sıkıntı yaratır diye random isimler veriyoruz ve exchange bağlıyoruz, subscribe(consumer) down olduğunda kuyruk böylelikle otomatik silinir. Böylelikle her ayrı proje ayrı kuyrukta veriyi alacak ama eğer belli bir projede veriyi çekeceksek kendimiz de isim verebiliriz ve bu kuyruğa gönderilmiş mesajlar kalıcı olacaktır ve bu kuyruğa gelen verileri consumer down olsa dahi başka bir consumerdan bu kuyruk adına ulaşarak veriyi alabiliriz.
-
                 channel.BasicQos(0, 1, false);//prefetchSize:0 yaptık yani consumer herhangi bir boyuttan veri alabilir.,prefetchCount: consumerların kaç tane mesaj alabileceğini belirtiyoruz. Bool Global ise: false ise kaçtane consumer varsa herbirine verdiğimiz  prefetchCount kadar mesaj iletilir. eğer false yaparsak verdiğimiz prefetchCount tüm consumerlara toplamı  prefetchCount kardar olacak kadar mesaj verilir. Örn: prefetchCount: verdik 3 consumer var. 2,2,1 şeklinde consumerlara toplam 5 olacak şekilde iletilir. Yani ne kadar çok subscriber(Consumer) proje ayaktaysa o kadar dinleme hızlı biter
+                var randomQueueName = channel.QueueDeclare().QueueName;//random olarak kuyruk adı oluşturur. İşi bitince silinir.
+
+                channel.QueueBind(randomQueueName, "logs-topic",routeKey,null);//Birden fazla  kez projeyi ayağı kaldırınca aynı kuyruk ismi olması sıkıntı yaratır diye random isimler veriyoruz ve exchange bağlıyoruz, subscribe(consumer) down olduğunda kuyruk böylelikle otomatik silinir. Böylelikle her ayrı proje ayrı kuyrukta veriyi alacak ama eğer belli bir projede veriyi çekeceksek kendimiz de isim verebiliriz ve bu kuyruğa gönderilmiş mesajlar kalıcı olacaktır ve bu kuyruğa gelen verileri consumer down olsa dahi başka bir consumerdan bu kuyruk adına ulaşarak veriyi alabiliriz.
+
+               
 
                 var consumer = new EventingBasicConsumer(channel);
 
@@ -37,9 +37,10 @@ namespace HelloWold.Subscriber//consumer diğer adı
                 consumer.Received += (object sender, BasicDeliverEventArgs e) =>
                     {
                         var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                        Console.WriteLine("Gelen Mesaj: " + message);
                         channel.BasicAck(e.DeliveryTag, false);//false yaptığımız autoAck işlemi için eğer mesaj teslim edildiyse kuyruktan sil demek
                         
-                        Console.WriteLine("Gelen Mesaj: " + message);
                     };// bu şekilde kullanım daha pratik
 
                 Console.ReadLine();
