c1775d5408d52f63a03656b4d94729dbab7cf992
sedat karasungur <limitless_fearless@hotmail.com>
2021-05-07 15:40:09 +0300

    4-ExchangeTypes(DirectExchange) bitti

--- HelloWold.Publisher/Program.cs
+++ HelloWold.Publisher/Program.cs
@@ -6,11 +6,18 @@ using System.Threading;
 
 namespace HelloWold.Publisher//producer diğer adı bu kısımda mesajı yayınlıyoruz
 {
+    public enum lognames
+    {
+        Critical=1,
+        Error=2,
+        Warning=3,
+        Info=4
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Thread.Sleep(1000);//projeyi aynı anda ayağıkaldırınca fanout mesajları subscriber(consumer) dinlemeden önce paylaşmamak için bu proje 1 sn sonra başlasın dedik.
+            Thread.Sleep(2000);//projeyi aynı anda ayağıkaldırınca fanout mesajları subscriber(consumer) dinlemeden önce paylaşmamak için bu proje 1 sn sonra başlasın dedik.
             var factory = new ConnectionFactory();
 
             factory.Uri = new Uri("amqps://orztfhbr:61JVQ4JIieuwkzllmolkOId430Hfzyv4@grouse.rmq.cloudamqp.com/orztfhbr");
@@ -19,23 +26,36 @@ namespace HelloWold.Publisher//producer diğer adı bu kısımda mesajı yayınl
             {
                 var channel = connection.CreateModel();//yeni bir kanal oluşturuyoruz
 
-                //FanoutExchange de kuyruğu producer(publisher) oluşturmaz. Consumer(subscriber) isterse veri için kuyruk oluşturur. Ve consumer kapandığında consumer ayarına göre kuyruk silinir. Örn: Hava saatlik hava durumu yayınlayan bir projem var exchange ile kaç kişi bu veriye ulaşacak bilmediğim için veriyi isteyen kişiler kuyruk oluşturup veriyi alabilir. Biz sadece yayınlamakla mükellefiz.Bu yüzden kuyruk oluşturma alanı yorum satırı haline getirildi. Fanout veriyi direkt olarak oluşturulmuş her kuyruğa verir. Yani tüm kuyruklar procuder(publisher) dan gelen veriyi aynı dinler. 
+                //Direct Exchange de kuyruğu ve exchangeyi producer(publisher) oluşturur. Direct veriyi ilgili olan oluşturulmuş  kuyruğa verir. Örn: Elimizde veri grupları var ve her bir veri grubu belli bir kuyruğa iletilecek şekildedir. Bu projemizde mesaj seviyeleri enum olarak kendimiz oluşturduk ve bu enum seviyelerine göre kuyruklara mesaj ileticez. Eğer aynı kuyruk için iki sunucu ayaktaysa ikisine de verileri farklı olarak atar. Yani ne kadar çok sunucu kuyruğu dinlerse o kadar hızlı dinleme biter
 
-                // channel.QueueDeclare("Yenikuyruk", true, false,false);//kuyrukadı,durable:false ise kuyruk memoryde tutulu true ise fiziksel olarak bir yere kaydolur. Biz true yaptık.,exclusive:true olursa sadece bu kanal üzerinden bu kuyruhğa erişilir. false ise başka kanallardan da erişilebilir ve biz false yaptık. autodelete: true ise eğer kuyruğa bağlı olan subscriber düşerse kuyruk silinsin demek.False yaptık. Kuyruk oluştur.
+               
 
-                channel.ExchangeDeclare("logs-fanout",type:ExchangeType.Fanout,durable:true);
+                channel.ExchangeDeclare("logs-direct", type: ExchangeType.Direct, durable: true);
 
-                Enumerable.Range(1, 50).ToList().ForEach(x=> {
+                Enum.GetNames(typeof(lognames)).ToList().ForEach(x=> {
+                    var routeKey = $"route-{x}";//hangi mesaj hangi kuyruğa gidecek burdan belirliyoruz. Yani key ile veriyi göndereceğimiz kuyruğu biliyoruz
+                    var queueName = $"direct-queue-{x}";//mesaj enumlarına göre bir kuyruklar oluşturduk
 
-                    string message = $"Message {x}";
+                    channel.QueueDeclare(queueName, true, false,false);//kuyrukadı,durable:false ise kuyruk memoryde tutulu true ise fiziksel olarak bir yere kaydolur. Biz true yaptık.,exclusive:true olursa sadece bu kanal üzerinden bu kuyruhğa erişilir. false ise başka kanallardan da erişilebilir ve biz false yaptık. autodelete: true ise eğer kuyruğa bağlı olan subscriber düşerse kuyruk silinsin demek.False yaptık. Kuyruk oluştur.
+
+                    channel.QueueBind(queueName, "logs-direct",routeKey, null);//kuyruğu keyi ile exchange tanıtıyoruz
+
+                });
+
+                Enumerable.Range(1, 50).ToList().ForEach(x =>
+                {
+                    lognames log =(lognames) new Random().Next(1, 5);// 50 mesaj gönderirken rastgele sevilerde gönderelim
+                    
+                    string message = $"Message-{log}";
 
                     var messagebody = Encoding.UTF8.GetBytes(message);
+                    var routeKey = $"route-{log}";
 
-                    channel.BasicPublish("logs-fanout","", null, messagebody);//exchange:logs-fanout , rouitingKey:"", IBasicProperties şimdilik null gönderelim. ve mesajımız
+                    channel.BasicPublish("logs-direct", routeKey,null, messagebody);//exchange:logs-direct , rouitingKey:routeKey, IBasicProperties şimdilik null gönderelim. ve mesajımız // exchange routekeye sahip olan kuyruğa mesajı iletiyor
                     Console.WriteLine("mesaj gönderildi.");
                 });
 
-                
+
                 Console.ReadLine();
             }
 
--- HelloWold.Subscriber/Program.cs
+++ HelloWold.Subscriber/Program.cs
@@ -19,27 +19,23 @@ namespace HelloWold.Subscriber//consumer diğer adı
             {
                 var channel = connection.CreateModel();
 
-
-
-                //Eğer burada da exchange oluşturursak hata vermez ama en azından fanout exchange için gereksiz
-
-                var randomQueueName = channel.QueueDeclare().QueueName;//random olarak kuyruk adı oluşturur.
-
-                channel.QueueBind(randomQueueName, "logs-fanout","",null);//Birden fazla  kez projeyi ayağı kaldırınca aynı kuyruk ismi olması sıkıntı yaratır diye random isimler veriyoruz ve exchange bağlıyoruz, subscribe(consumer) down olduğunda kuyruk böylelikle otomatik silinir. Böylelikle her ayrı proje ayrı kuyrukta veriyi alacak ama eğer belli bir projede veriyi çekeceksek kendimiz de isim verebiliriz ve bu kuyruğa gönderilmiş mesajlar kalıcı olacaktır ve bu kuyruğa gelen verileri consumer down olsa dahi başka bir consumerdan bu kuyruk adına ulaşarak veriyi alabiliriz.
-
                 channel.BasicQos(0, 1, false);//prefetchSize:0 yaptık yani consumer herhangi bir boyuttan veri alabilir.,prefetchCount: consumerların kaç tane mesaj alabileceğini belirtiyoruz. Bool Global ise: false ise kaçtane consumer varsa herbirine verdiğimiz  prefetchCount kadar mesaj iletilir. eğer false yaparsak verdiğimiz prefetchCount tüm consumerlara toplamı  prefetchCount kardar olacak kadar mesaj verilir. Örn: prefetchCount: verdik 3 consumer var. 2,2,1 şeklinde consumerlara toplam 5 olacak şekilde iletilir. Yani ne kadar çok subscriber(Consumer) proje ayaktaysa o kadar dinleme hızlı biter
 
                 var consumer = new EventingBasicConsumer(channel);
 
-                channel.BasicConsume(randomQueueName, false, consumer);//kuyruk adı,autoAck:false Work Queue konusu için false yaptık ve eventte yeni tanım var
+                var queueName = "direct-queue-Critical";// publishte yayınladığımız mesajlardan istediğimiz kuyrukltan veri çekebiliriz. Biz Bu kuyruktan çekelim
+                channel.BasicConsume(queueName, false, consumer);//kuyruk adı,autoAck:false Work Queue konusu için false yaptık ve eventte yeni tanım var
                 Console.WriteLine("Mesajlar dinleniyor");
 
                 consumer.Received += (object sender, BasicDeliverEventArgs e) =>
                     {
                         var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                        Console.WriteLine("Gelen Mesaj: " + message);
+
                         channel.BasicAck(e.DeliveryTag, false);//false yaptığımız autoAck işlemi için eğer mesaj teslim edildiyse kuyruktan sil demek
                         
-                        Console.WriteLine("Gelen Mesaj: " + message);
+                        // istersek burada aldığımız mesajları bir yere kaydedebiliriz.
+                        
                     };// bu şekilde kullanım daha pratik
 
                 Console.ReadLine();
